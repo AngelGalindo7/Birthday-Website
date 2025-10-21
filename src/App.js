6207bb63ac8df63e33f9f65a1ca05699bc1035cb
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-10-21 03:20:18 -0700

    Updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
-
+import AudioPlayer from './components/main page/AudioPlayer';
+import ConfettiButton from './components/main page/Confetti'
+import Background from "./components/main page/Background";
+import CakeImage from "./components/main page/Cake";
+import LightsToggle from "./components/LightsToggle";
+import Overlay from "./components/main page/Overlay";
+import Cake from "./components/main page/CakeContainer";
+import ConfettiAuto from "./components/main page/Confetti";
+import VoiceTestLogger from "./components/main page/voicetest";
 function App() {
+  const [lightsOff, setLightsOff] = useState(false);
+  const [stage, setStage] = useState("music");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+      <Background>
+      {
+      <AudioPlayer
+      visible={ stage === "music"}
+      onDone = {() => setStage("candles")}
+      />
+      
+      }
+
+      {
+        stage === "candles" && (
+          <div className="">
+            <Cake />
+            <CakeImage />
+            <ConfettiAuto/>
+            <button onClick={() => setStage("outro")}>Go to Outro</button>
+          </div>
+        )
+      }
+
+   
+      
+  <LightsToggle lightsOff={lightsOff} setLightsOff={setLightsOff}/>
+      <Overlay isDark={lightsOff} />
+      </Background>
+      
+    );
 }
 
 export default App;
