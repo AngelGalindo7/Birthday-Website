9947a1659c7955afaa0910e004ef4c771c60ffd9
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-10-20 17:08:36 -0700

    Added prop function to let parent know to move onto the next stage

--- src/components/main page/AudioPlayer.js
+++ src/components/main page/AudioPlayer.js
@@ -1,6 +1,5 @@
 import React, {useRef, useState, useEffect} from "react";
 
-
 //Look into normalizing audio file names when non ascii characters appear
 function importAll(r){
   return r.keys().map(r);
@@ -10,15 +9,29 @@ function importAll(r){
 //consider adding to public to avoid adding bundle size in deployment
 const songs = importAll(require.context("../../assets/audio",false,/\.mp3$/));
 
-export default function AudioPlayer() {
+export default function AudioPlayer( {onDone, visible} ) {
   const audioRef = useRef(null);
-
+  const [started, setStarted] = useState(false);
   const [currentSongIndex,setSongIndex] = useState(0);
 
-  useEffect(() => {
+
+  const handlePlay = async () => {
+    setStarted(true);
     const audio = audioRef.current;
     if (!audio) return;
 
+    try {
+      await audio.play();
+
+      setTimeout(() => onDone && onDone(), 4000);
+    } catch(err) {
+      console.warn("Play blocked", err);
+    }
+  };
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (!audio) return;
+    
     const handleEnded = () => {
       setSongIndex((prev) => (prev + 1) % songs.length);
     };
@@ -31,14 +44,20 @@ export default function AudioPlayer() {
   }, []);
 
   useEffect ( () => {
+    
     const audio = audioRef.current;
     if (!audio) return;
     audio.src = songs[currentSongIndex];
     audio.load();
-    //consider playing audio automatically instead of on click
-    audio.preload = "auto";
-    audio.autoplay = true;
-  }, [currentSongIndex]);
+
+    if (started){
+      const playPromise = audio.play();
+      if(playPromise !== undefined) {
+        playPromise.catch((err) => console.warn("Autoplay blocked:", err));
+      }
+    }
+    
+  }, [currentSongIndex, started]);
  
 
   const pauseAudio = () => audioRef.current.pause();
@@ -46,14 +65,25 @@ export default function AudioPlayer() {
   const skipPrev = () => setSongIndex((i) => (i - 1 + songs.length) % songs.length);
 
   return (
-    <div className="audio-player">
-      <h2>Now playing: Song {currentSongIndex + 1}</h2>
-      <audio ref={audioRef} controls preload="auto" />
-      <div className="audio-buttons">
+    <div className="audio-player"
+      style = {{display : visible ? "block" : "none"}}
+      >
+      <audio 
+      ref={audioRef} 
+      controls 
+      preload="auto"
+      style={{display: started ? "inline": "none"}} />
+
+      {!started && <button className="shadow__btn fade-in" onClick={handlePlay}>音楽を再生する</button>}
+      
+      {started && (
+        <div className="audio-buttons">
         <button onClick={skipPrev}>Prev</button>
         <button onClick={pauseAudio}>Pause</button>
         <button onClick={skipNext}>Next</button>
       </div>
+      )}
+
     </div>
   );
 }
