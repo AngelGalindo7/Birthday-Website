580cefbf3d3df8b017baa0b6c0a74f97f12f16ee
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-10-21 04:15:33 -0700

    Renamed image file

--- src/components/main page/Cake.js
+++ src/components/main page/Cake.js
@@ -4,7 +4,7 @@ export default function CakeImage() {
     return (
         <div className="cake-container">
             <img
-            src={`${process.env.PUBLIC_URL}/Untitled_Artwork-1 (1).PNG`}
+            src={`${process.env.PUBLIC_URL}/caketmp..PNG`}
             alt="Birthday Cake"
             className="cake"
             />
