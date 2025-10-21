34ad1df5b65eb4abb80f834d83b35a94c82c5016
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-10-20 17:05:28 -0700

    Cake rendering component

---
+++ src/components/main page/Cake.js
@@ -0,0 +1,13 @@
+
+
+export default function CakeImage() {
+    return (
+        <div className="cake-container">
+            <img
+            src="cakeimage.PNG"
+            alt="Birthday Cake"
+            className="cake"
+            />
+        </div>
+    );
+}
