c7c4646705df9fb30225623a8f04bf9158c377af
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-10-22 22:56:52 -0700

    Updated background to add verical frame and centered overlay

--- src/components/main page/Background.js
+++ src/components/main page/Background.js
@@ -7,10 +7,19 @@ export default function Background({children}) {
         <div
             className="background"
             style={{
-                backgroundImage: `url(${process.env.PUBLIC_URL}/tmpart_bday.PNG)`
+                //backgroundImage: `url(${process.env.PUBLIC_URL}/tmpart_bday.PNG)`
+                backgroundColor: "#000000", // fallback color
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                overflow: "hidden"
             }}
-        >
+        >   
+        <div className="vertical-frame">
+        <div className ="centered-overlay">
             {children}
+         </div>
+        </div>
         </div>
     );
 }
