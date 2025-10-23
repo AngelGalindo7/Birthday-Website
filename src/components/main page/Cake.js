7d9c25469ed38465d4b534ec20738345502ede1f
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-10-22 23:19:44 -0700

    Typo with PNG instead of png

--- src/components/main page/Cake.js
+++ src/components/main page/Cake.js
@@ -4,7 +4,7 @@ export default function CakeImage( {fadeOut}) {
     return (
         <div className={`cake-container ${fadeOut ? "fade-out" : ""}`}>
             <img
-            src={`${process.env.PUBLIC_URL}/BdayImage.PNG`}
+            src={`${process.env.PUBLIC_URL}/BdayImage.png`}
             alt="Birthday Cake"
             className="cake"
             />
